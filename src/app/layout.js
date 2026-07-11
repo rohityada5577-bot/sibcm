eb23735108819af8701d4e1a8b1a1668bfcc6017
Rohit Yadav <rohityada5577@gmail.com>
2026-07-11 18:05:05 +0530

    boostrap library

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,18 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap-icons/font/bootstrap-icons.css";
-// import "./globals.css";
-
-// import Navbar from "@/components/layout/Navbar";
-// import Footer from "@/components/layout/Footer";
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <Navbar />
-//         {children}
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapClient from "@/components/BootstrapClient";
 import Navbar from "@/components/layout/Navbar";
- import Footer from "@/components/layout/Footer";
-import "bootstrap-icons/font/bootstrap-icons.css";
- import "./globals.css";
-
+import Footer from "@/components/layout/Footer";
+import SiboAssistant from "@/components/common/SiboAssistant";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <BootstrapClient />
         <Navbar />
         {children}
+        {/* <SiboAssistant /> */}
         <Footer />
       </body>
     </html>
