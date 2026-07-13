867d2c9002dc5c033f7fea36e7021fa89de47613
Rohit Yadav <rohityada5577@gmail.com>
2026-07-13 16:47:37 +0530

    changes data

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -2,7 +2,7 @@ import PricingHero from "@/components/pricing/PricingHero";
 import PricingCards from "@/components/pricing/PricingCards";
 import PricingCustomROI from "@/components/pricing/PricingCustomROI";
 import PricingInclusions from "@/components/pricing/PricingInclusions";
-import FaqSection from "@/components/common/FaqSection";
+// import FaqSection from "@/components/common/FaqSection";
 
 
 
